8573f07d9b624367a38a3c8bc7dfa8314e464abf
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-07-30 17:56:08 -0700

    added varibles and figured out how to generate random variable from array

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(computerChoices);
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+
+var getRandomLetter = Math.floor(Math.random(computerChoices));
+
+
+
+
+// When User gets on page, generate a random letter from the Index of computerChoices
+
+
+
+
+
+
