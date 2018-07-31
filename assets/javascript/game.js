ec31dbac410502d7a910fc5ce755a0617d9877db
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-07-31 15:40:14 -0700

    added if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,47 @@
+
+// Create an array for the computer to choose a random letter
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 console.log(computerChoices);
+
+// Create varibles
 var wins = 0;
 var losses = 0;
 var numGuesses = 9;
 
-var getRandomLetter = Math.floor(Math.random(computerChoices));
+// Thid function is run whenever the user presses a key
+    document.onkeyup = function(event) {
 
+        //Determmines which key was pressed
+        var userGuess = event.key;
+        console.log(event.key)
+        
+        // randomly chooses a letter from the computerChoice array. This is the computers guess
+        var getRandomLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(getRandomLetter)
+     
+        //Logic if the computer and user guess the same key
+        if(userGuess) {
+            numGuesses--;
+        }
 
+        if (userGuess == getRandomLetter) {
+            wins++;
+        }
+        else if( userGuess !== getRandomLetter) {
+            losses--;
+        }
 
+        var html =
+          "<p>You chose: " + userGuess + "</p>" +
+          "<p>The computer chose: " + computerGuess + "</p>" +
+          "<p>wins: " + wins + "</p>" +
+          "<p>losses: " + losses + "</p>" +
+          "<p>number of gussess left" + numGuesses + "</p>"
 
-// When User gets on page, generate a random letter from the Index of computerChoices
-
-
-
-
-
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+      };
+   
+      
+  </script>
 
