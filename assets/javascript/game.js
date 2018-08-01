83c59481f30da31b5e14e74ac1c6c23951aac60a
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-07-31 21:49:24 -0700

    Final clean

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 // Create varibles
 var wins = 0;
 var losses = 0;
-var numberOfGuessesLeft = 9;
+var guessesRemaining = 9;
 // an array that will hold all of the users guesses
 var guessedLetters = [];
 // what the user picks by pressing a key
@@ -24,11 +24,12 @@ var userGuess = [];
 
         // randomly chooses a letter from the computerChoice array. This is the computers guess
         var letterToBeGuessed = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(letterToBeGuessed)
        
 
         //Clears the amount Guess Key Letters when they reach the end of their left # of guesses left
         var reset = function () {
-        guessesRemaining = ;
+        guessesRemaining = 9;
         guessedLetters = [];
       }
        
@@ -37,28 +38,31 @@ var userGuess = [];
         //Logic if the computer and user guess the same key
         if (letterToBeGuessed == userGuess) {
             wins++;
-            numberOfGuessesLeft: 9;
-        }
-
-        else {
-            userGuess = guessedLetters;
-            numberOfGuessesLeft--
-            };
+            reset ();
 
         //If the user's guess equals 0 and runs out, the user loses and their guesses will start over for the user to try again 
-        if (numberOfGuessesLeft == 0) {
+       
+        } else {
+            guessesRemaining--;
+        }
+
+
+        //If the user's guess equals 0 and runs out, the user loses and their guesses will start over for the user to try again 
+        if (guessesRemaining == 0) {
         losses++;
-        numberOfGuessesLeft = 9;
+        guessesRemaining = 9;
         reset();
-      }
+                };
+     
+    
         
 
         var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>number of gussess left: " + numberOfGuessesLeft + "</p>"
-          "<p>Guesses so Far: " + guessedLetters + "<p>"
+          "<p>Your Guesses so Far: " + guessedLetters + "</p>" +
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Number of Guesses Left: " + guessesRemaining + "</p>"
+        //   "<p>Guesses so Far: " + guessedLetters + "<p>"
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
