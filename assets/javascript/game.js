be1a67bc44eaccd8ca731a54012e346edc8fa20d
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-07-31 20:33:43 -0700

    Able to push user to empty array guess guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,42 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 // Create varibles
 var wins = 0;
 var losses = 0;
-var numGuesses = 9;
-var guessesSoFar = []
+var numberOfGuessesLeft = 9;
+// an array that will hold all of the users guesses
+var guessedLetters = [];
+// what the user picks by pressing a key
+var userGuess = [];
 
- // randomly chooses a letter from the computerChoice array. This is the computers guess
- var getRandomLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
- console.log(getRandomLetter)
 
-// Thid function is run whenever the user presses a key
+
+// This function is run whenever the user presses a key
     document.onkeyup = function(event) {
 
-        //Determmines which key was pressed
+        //Gathers Users guessed key letters
         var userGuess = event.key;
-        console.log(event.key);
-        
-     
+        guessedLetters.push(userGuess);
+        console.log(guessedLetters);
+
+        // randomly chooses a letter from the computerChoice array. This is the computers guess
+        var letterToBeGuessed = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(letterToBeGuessed);
+       
+      
      
         //Logic if the computer and user guess the same key
         if(userGuess) {
-            numGuesses--;
+            numberOfGuessesLeft--;
+        }
+    
+
+        if (letterToBeGuessed == userGuess) {
+            wins++;
         }
 
-        if (userGuess == getRandomLetter) {
-            wins++;
-    
-        }
-        else if(userGuess !== getRandomLetter) {
+        else if(letterToBeGuessed !== userGuess) {
             losses++;
+            userGuess = guessedLetters;
+            guessedLetters.push(userGuess);
             };
         
 
@@ -40,13 +49,11 @@ var guessesSoFar = []
           "<p>You chose: " + userGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
           "<p>losses: " + losses + "</p>" +
-          "<p>number of gussess left: " + numGuesses + "</p>"
-          "<p>Guesses so Far: " + guessesSoFar + "<p>"
+          "<p>number of gussess left: " + numberOfGuessesLeft + "</p>"
+          "<p>Guesses so Far: " + guessedLetters + "<p>"
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
-      };
-function newFunction() {
-    console.log(computerChoices);
-}
+        }
+
 
